ea521d4add695ba8956ec649939f10bbccd01a80
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-10-31 10:56:14 -0400

    Update mic-volume.sh

--- mic-volume/mic-volume.sh
+++ mic-volume/mic-volume.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-DEFAULT_SOURCE_INDEX=$(pacmd list-sources | grep "\* index:" | cut -d' ' -f5)
+DEFAULT_SOURCE=$(pactl get-default-source)
 
 display_volume() {
 	if [ -z "$volume" ]; then
 	  echo "No Mic Found"
 	else
-	  volume="${volume//[[:blank:]]/}" 
+          volume=${volume% /*}
+          volume=${volume#*/ }
+          volume=${volume#*/ }
+          volume=${volume#*/ }
+	  volume="${volume//[[:blank:]]/}"
+          mute=${mute#*: }
 	  if [[ "$mute" == *"yes"* ]]; then
 	    echo "[$volume]"
 	  elif [[ "$mute" == *"no"* ]]; then
@@ -20,32 +25,32 @@ display_volume() {
 case $1 in
 	"show-vol")
 		if [ -z "$2" ]; then
-  			volume=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 7 | grep "volume" | awk -F/ '{print $2}')
-  			mute=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 11 | grep "muted")
+  			volume=$(pactl get-source-volume $DEFAULT_SOURCE)
+  			mute=$(pactl get-source-mute $DEFAULT_SOURCE)
 			display_volume
 		else
-  			volume=$(pacmd list-sources | grep "$2" -A 6 | grep "volume" | awk -F/ '{print $2}')
-  			mute=$(pacmd list-sources | grep "$2" -A 11 | grep "muted" )
+  			volume=$(pactl get-source-volume $2)
+  			mute=$(pacmd get-source-mute $2)
 			display_volume
 		fi
 		;;
 	"inc-vol")
 		if [ -z "$2" ]; then
-			pactl set-source-volume $DEFAULT_SOURCE_INDEX +7%
+			pactl set-source-volume $DEFAULT_SOURCE +7%
 		else
 			pactl set-source-volume $2 +7%
 		fi
 		;;
 	"dec-vol")
 		if [ -z "$2" ]; then
-			pactl set-source-volume $DEFAULT_SOURCE_INDEX -7%
+			pactl set-source-volume $DEFAULT_SOURCE -7%
 		else
 			pactl set-source-volume $2 -7%
 		fi
 		;;
 	"mute-vol")
 		if [ -z "$2" ]; then
-			pactl set-source-mute $DEFAULT_SOURCE_INDEX toggle
+			pactl set-source-mute $DEFAULT_SOURCE toggle
 		else
 			pactl set-source-mute $2 toggle
 		fi
